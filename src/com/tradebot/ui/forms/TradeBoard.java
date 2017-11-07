e5ea5b50ada94a592697e9f83e99265620cc2ad6
Sivaanand Murugesan <sivaanand.m@gmail.com>
2017-11-08 00:05:07 +0530

    Progress on db creation and so on

--- src/com/tradebot/ui/forms/TradeBoard.java
+++ src/com/tradebot/ui/forms/TradeBoard.java
@@ -3,9 +3,15 @@ package com.tradebot.ui.forms;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
 
 public class TradeBoard {
 
@@ -40,15 +46,47 @@ public class TradeBoard {
 	private void initialize() {
 		
 		frmTradeBoard = new JFrame();
-		frmTradeBoard.setBounds(100, 100, 450, 300);
 		frmTradeBoard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmTradeBoard.setBackground(new Color(36,34,29));
 		frmTradeBoard.getContentPane().setBackground(new Color(51, 51, 51));
 		
+		
+		//maximize the window 
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	    double width = screenSize.getWidth();
 	    double height = screenSize.getHeight();
 	    frmTradeBoard.setSize((int)width, (int)height);
+	    
+	    JPanel pnltotal = new JPanel();
+	    pnltotal.setBackground(new Color(36,34,29));
+		frmTradeBoard.getContentPane().add(pnltotal, BorderLayout.CENTER);
+		pnltotal.setLayout(new BorderLayout(0, 0));
+		//heading label
+		JLabel lblLiveTradeDashboard = new JLabel("Live Trade Dashboard");
+		lblLiveTradeDashboard.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLiveTradeDashboard.setFont(new Font("Tsukushi A Round Gothic", Font.BOLD, 26));
+		lblLiveTradeDashboard.setForeground(new Color(240,159,108));
+		pnltotal.add(lblLiveTradeDashboard, BorderLayout.NORTH);
+		
+		//Center panel for grid 
+		JPanel pnlgridpanel = new JPanel();
+		pnlgridpanel.setBackground(new Color(36,34,29));
+		pnltotal.add(pnlgridpanel, BorderLayout.CENTER);
+		
+		
+		
+		
+		//Botom panel design		
+		JPanel pnlbottom = new JPanel();
+		pnlbottom.setBackground(Color.GREEN);
+		pnltotal.add(pnlbottom, BorderLayout.SOUTH);
+		
+		
+		
+		
+		
+		
+	    
 	    
 	}
 
